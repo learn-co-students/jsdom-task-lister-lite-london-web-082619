1fdecfbdf2cddd36faea0cb7cea423640214a104
Abdullah Bajaman <ambajaman@gmail.com>
2019-10-08 15:00:18 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('#create-task-form');
+  const list = document.querySelector('#tasks');
+  
+  
+  form.addEventListener('submit', e=>{
+    e.preventDefault();
+    // grab the value from text field in the form 
+    let value = e.target[0].value
+
+    
+    
+    const task = document.querySelector(value);
+    if (value === ""){return} //make the text field empty
+    if(!task){
+      let li = document.createElement('li');
+      li.duration = e.target[1].value;
+      li.priority = e.target[2].value;
+      setPriorityColor(li)
+
+      let btn = document.createElement('button');
+      btn.innerHTML = "❌";
+
+      list.appendChild(li)
+      li.id = value 
+      li.innerHTML = `${value} - ${li.duration} min`
+      li.appendChild(btn);
+      btn.addEventListener('click', e=>{
+        btn.parentElement.remove();
+      })
+    }
+    value = ""
+
+  })
+
+
+  function setPriorityColor(taskItem){
+    if (taskItem.priority == 'high'){
+      taskItem.style.color = 'red'
+    }else if (taskItem.priority == 'medium'){
+      taskItem.style.color = 'orange'
+    } else{
+      taskItem.style.color = 'green'
+    }
+  }
 });
