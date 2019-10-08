003a2ee63b5b2bb01b7902fd7c21c804be51f085
github username <github email address>
2019-10-08 15:29:37 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  form = document.getElementById('create-task-form')
+
+  form.addEventListener('submit', addTask)
+
+  function addTask(e){
+      e.preventDefault();
+
+      let ul = document.getElementById('tasks')
+      let li = document.createElement('li')
+      li.innerHTML = document.getElementById('new-task-description').value + " "
+      ul.appendChild(li)
+      deleteButton(li)
+
+      // take input from submit
+      // let li = createElement('li')
+      // li.innerHTML = input
+      // store input in id=list').appendChild(li)
+  }
+
+  function deleteButton(item){
+      let deleteButton = document.createElement('button')
+      deleteButton.innerHTML = 'delete';
+      deleteButton.addEventListener('click', removeItem)
+      item.appendChild(deleteButton)  
+  }
+
+  function removeItem(element){
+    let parentUl = element.path[2]
+    let childLi = element.path[1]
+    parentUl.removeChild(childLi)
+  }
+
+
 });
